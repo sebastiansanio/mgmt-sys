07af795c1b9869b1e45d806b6d90b0d127926de8
sebastian <sebastiansanio@outlook.com>
2015-12-21 22:07:40 -0300

    Corrección de neteos.

--- grails-app/controllers/mgmt/balance/NettingController.groovy
+++ grails-app/controllers/mgmt/balance/NettingController.groovy
@@ -10,12 +10,12 @@ class NettingController {
     def index() {
 		Map results = new HashMap()
 		def sql = new Sql(dataSource)
-		def row = sql.firstRow("select sum(amount+iibb+other_perceptions) as amount,sum(iva) as iva from movement_item where concept_id in (select id from concept where code between 'P800' and 'P810')")
+		def row = sql.firstRow("select sum(coalesce(amount,0)+coalesce(iibb,0)+coalesce(other_perceptions,0)) as amount,sum(coalesce(iva,0)) as iva from movement_item where concept_id in (select id from concept where code between 'P800' and 'P810')")
 		results.incomeAmount = row.amount
 		results.incomeIva = row.iva
 		sql.close()
 		
-		row = sql.firstRow("select sum(amount+iibb+other_perceptions) as amount,sum(iva) as iva from movement_item where concept_id in (select id from concept where code between 'M800' and 'M810')")
+		row = sql.firstRow("select sum(coalesce(amount,0)+coalesce(iibb,0)+coalesce(other_perceptions,0)) as amount,sum(coalesce(iva,0)) as iva from movement_item where concept_id in (select id from concept where code between 'M800' and 'M810')")
 		results.expensesAmount = row.amount
 		results.expensesIva = row.iva
 		sql.close()
